ba3e7a63ea6bb8ddf486201adf2dcde9507667d2
davub <davub11@gmail.com>
2020-12-21 13:34:23 +0100

    implemented calculation of keeping k from n same dice with arbitrary probability distribution works up to k = 2 logic errer for k > 2

--- probability_calculation/x_keep_y.py
+++ probability_calculation/x_keep_y.py
@@ -43,42 +43,79 @@ def n_keep_k_highest(n, k, values, probs):
     result = {}
     value_num = len(values)
     sum_combination_perms = list(itertools.product(range(value_num), repeat=k))
-    num_poss_keep_dice = scipy.special.binom(n, k)
+    sum_combination_perms = list(itertools.combinations_with_replacement(range(value_num), k))
 
     for perm in sum_combination_perms:
+        min_value = np.min(values[list(perm)])
+        num_dice_equals_min = 0
         cur_sum = 0
         cur_prob = 1
         for i in perm:
             cur_sum += values[i]
-            cur_prob *= probs[i]
+            if values[i] == min_value:
+                num_dice_equals_min += 1
+            else:
+                cur_prob *= probs[i]
 
-        prob_rest_dice = get_prob_smaller_than(n-k, values[min(perm)], values, probs)
-        cur_prob *= prob_rest_dice * num_poss_keep_dice
-        print(cur_sum, prob_rest_dice)
+        prob_rest_dice = get_prob_smaller_than(n-k+num_dice_equals_min, values[min(perm)], values, probs, num_equal_dice=num_dice_equals_min)
+        num_poss_keep_dice = scipy.special.binom(n, k-num_dice_equals_min)
+        total_prob = cur_prob * prob_rest_dice * num_poss_keep_dice
+        print(perm, values[list(perm)], min_value, cur_sum, cur_prob, prob_rest_dice, num_poss_keep_dice, num_dice_equals_min)
 
         if cur_sum in result.keys():
-            result[cur_sum] += cur_prob
+            result[cur_sum] += total_prob
         else:
-            result[cur_sum] = cur_prob
+            result[cur_sum] = total_prob
 
     return result
 
 
-def get_prob_smaller_than(dice_num, value, values, probs):
+def get_prob_smaller_than(dice_num, value, values, probs, also_equals=True, num_equal_dice=0):
     values = np.asarray(values)
     probs = np.asarray(probs)
 
     value_idx = np.where(values == value)[0][0]
-    prob_lower = np.sum(probs[:value_idx+1])
+    if also_equals:
+        value_idx += 1
+    prob_lower = np.sum(probs[:value_idx])
     prob_all_lower = prob_lower ** dice_num
+
+    if num_equal_dice > 0:
+        prob_all_lower_not_equal = get_prob_smaller_than(dice_num, value, values, probs, also_equals=False)
+        prob_all_lower -= prob_all_lower_not_equal
+        for i in range(1, num_equal_dice):
+            prob_n_i_lower = get_prob_smaller_than(dice_num-i, value, values, probs, also_equals=False)
+            prob_i_equals = probs[value_idx-1] ** i
+            num_poss_i_equals = scipy.special.binom(dice_num, i)
+            prob_all_lower_except_i = prob_n_i_lower * prob_i_equals * num_poss_i_equals
+            prob_all_lower -= prob_all_lower_except_i
+            print(dice_num, i, prob_all_lower_except_i, prob_n_i_lower, prob_i_equals, num_poss_i_equals)
 
     return prob_all_lower
 
 
-values = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+def get_prob_smaller_min_one_equal(dice_num, x, values, probs):
+    values = np.asarray(values)
+    probs = np.asarray(probs)
+    value_idx = np.where(values == x)[0][0]
+
+    prob_all_smaller = get_prob_smaller_than(dice_num, x, values, probs, also_equals=True)
+
+    # transform probs into world with no value higher than x
+    values_x = values[:x+1]
+    probs_x = probs[:x+1]
+    probs_x = probs_x / np.sum(probs_x)
+
+    prob_die_is_smaller = np.sum(probs[:-1])
+    prob_min_one_is_equal = 1 - (prob_die_is_smaller ** dice_num)
+
+    return prob_min_one_is_equal
+
+
+values = np.array([-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]) + 4
 probs = [1/12]*12
 
-probabilities = n_keep_k_highest(3, 1, values, probs)
+probabilities = n_keep_k_highest(4, 2, values, probs)
 
 mean = 0
 for sum, prob in probabilities.items():
